f586f93e32cf49002c230af92db294ca0b979c7f
crisefd <cristhian.fuertes@correounivalle.edu.co>
2016-03-20 13:30:06 -0500

    se organiza el flapper-news para que funcione con rails

---
+++ rails-flapper-news/app/assets/javascripts/posts/posts.js
@@ -0,0 +1,5 @@
+app.factory('posts', function(){
+  return{
+      p : [{title:'Google', link:'www.google.com', upvotes: 0, comments:[]}]
+    }
+});
---
+++ rails-flapper-news/app/assets/javascripts/posts/postsCtrl.js
@@ -0,0 +1,22 @@
+app.controller('PostsCtrl', [
+              '$scope',
+              '$stateParams',
+              'posts',
+              function($scope, $stateParams, posts){
+                $scope.post = posts.p[$stateParams.id];
+                //alert($scope.post.comments);
+                $scope.addComment = function(){
+                  if($scope.body === ''){ return; }
+                  $scope.post.comments.push(
+                      {
+                        body: $scope.body,
+                        author: 'user',
+                        upvotes: 0
+                      });
+                  $scope.body = '';
+                };
+                $scope.incrementUpvotesComment = function(comment) {
+                  comment.upvotes += 1;
+                };
+              }
+            ]);
